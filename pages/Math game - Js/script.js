8f6d61707b7aebdbcbbb4831aa66610cdf705f38
Sameer <72139940+sameercreates@users.noreply.github.com>
2025-07-13 17:03:18 +0530

    removed unnecessary clutter

--- pages/Math game - Js/script.js
+++
@@ -1,9 +0,0 @@
-var play = document.querySelector("#play");
-
-function loadGameScreen() {
-  window.location.href = "/pages/game.html";
-}
-
-function comingSoon() {
-  window.location.href = "/pages/coming_soon.html";
-}
